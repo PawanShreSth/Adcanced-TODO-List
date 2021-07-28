ef930b3a69cea3a9c6b1d0a2dd4586bdb90e8f11
Pawan Shrestha <pawanshresthait@gmail.com>
2021-07-28 11:40:42 +0545

    Loading saved todos from local storage

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const form = document.querySelector('#new-todo-form');
 const todoInput = document.querySelector('#todo-input');
 const template = document.querySelector('#list-item-template');
 const list = document.querySelector('#list')
-let todos = [];
 const LOCAL_STORAGE_PREFIX = 'ADVANCED_TODO_LIST';
 const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`;
+let todos = loadTodos();
+
+todos.forEach(renderTodo)
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -28,6 +30,11 @@ function renderTodo(todoName) {
 
     textElement.innerText = todoName;
     list.appendChild(templateClone)
+}
+
+function loadTodos() {
+    const todosString = localStorage.getItem(TODOS_STORAGE_KEY);
+    return JSON.parse(todosString) || [];
 }
 
 function saveTodos() {
